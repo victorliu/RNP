fc308958436634a3fdccafc8c1b4d9abd0d371eb
Victor Liu <victor.liu@gmail.com>
2012-05-07 18:06:53 -0700

    Added Tuning header.

---
+++ inc/Tuning.h
@@ -0,0 +1,10 @@
+#ifndef RNP_TUNING_H_INCLUDED
+#defien RNP_TUNING_H_INCLUDED
+
+namespace RNP{
+namespace Tuning{
+	static const size_t MemoryAlignment = 16u;
+}; // namespace Tuning
+}; // namespace RNP
+
+#endif // RNP_TUNING_H_INCLUDED
